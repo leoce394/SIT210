5640e038c8dd9fb5e75f7df40c023a750d68420a
leoce394 <s225198396@deakin.edu.au>
2026-05-16 15:10:25 +1000

    Add files via upload

---
+++ 8.1HD Code/8.1HD.py
@@ -0,0 +1,40 @@
+import speech_recognition as speech
+import asyncio
+from bleak import BleakScanner, BleakClient
+
+voiceRec = speech.Recognizer()
+DEVICE = "ArduinoNano"
+UUID = "19B10001-EB67-592D-B9AC-A76E4D89BC63"
+running = True
+
+async def send(command):
+    arduino=await BleakScanner.find_device_by_name(DEVICE)
+    if arduino is none:
+        print("Error with finding arduino")
+        return
+    else:
+        async with BleakClient(arduino) as client:
+            await client.write_gatt_char(COMMAND, command.encode("utf-8"), response=True)
+            print("Sent: ",command)
+
+while (running):
+    with speech.Microphone() as source:
+        
+        voiceRec.adjust_for_ambient_noise(source, duration=1)
+        
+        print("Say a command...")
+        audio =voiceRec.listen(source)
+
+    try:
+        text = voiceRec.recognize_google(audio)
+        text = text.lower()
+        print("You said: ", text)
+        if (text == "quit"):
+            running=False
+        else:
+            asyncio.run(send(text))
+        
+    except speech.UnknownValueError:
+        print("Could not understand audio")
+    except speech.RequestError:
+        print("Speech recognition  service error")
